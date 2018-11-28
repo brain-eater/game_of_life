da9d870aa4f59600207d238e8cb1976dd62f0e54
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-11-28 15:07:10 +0530

    Introduced displayGrid

--- src/util.js
+++ src/util.js
@@ -6,6 +6,17 @@ const generate2DGrid = function(rows,columns){
   return grid;
 }
 
+const displayGrid = function(gridValues) {
+  let grid = [];
+  for (let row = 0; row < gridValues.length; row++) {
+    let justifiedCells = gridValues[row].map(x=>' '+x+' ').join('|');
+    grid.push(justifiedCells);
+  }
+  return grid.join('\n-----------\n');
+};
+
+exports.displayGrid = displayGrid;
+
 exports.generate2DGrid = generate2DGrid;  
 
 
--- test/utilTest.js
+++ test/utilTest.js
@@ -1,8 +1,16 @@
 const deepEqual = require('assert').deepEqual;
-const {generate2DGrid} = require('../src/util.js');
+const {displayGrid,
+       generate2DGrid} = require('../src/util.js');
 
 describe( 'generate2DGrid' , function() {
   it( 'should return a grid of given row and column' , function() {
     deepEqual(generate2DGrid(2,2),[[0,0],[0,0]]);
   });
 })
+
+describe('displayGrid', function() {
+  it('should return string representing grid', function() {
+    deepEqual(displayGrid([[1], [1]]), ' 1 \n-----------\n 1 ');
+    deepEqual(displayGrid([[1, 2], [1, 2]]), ' 1 | 2 \n-----------\n 1 | 2 ');
+  });
+});
